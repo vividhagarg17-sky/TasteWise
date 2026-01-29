97ae8f64422b9abb52e1e4512647e488e59ae231
Srishti Srivastava <srishti.school2010@gmail.com>
2026-01-29 18:19:48 +0530

    another commit for index.tsx

--- SmartBite/app/index.tsx
+++ SmartBite/app/index.tsx
@@ -1,12 +1,44 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { Text, View, StyleSheet,Image} from "react-native";
+import { useNavigation } from "expo-router";
 
 const Index = () => {
+const navigation = useNavigation();
+
+useEffect(() => {
+  const timer = setTimeout(() => {
+    navigation.navigate("home"); // Navigate to the home screen after 10 seconds
+  }, 10000);
+
+  return () => clearTimeout(timer); // Clear the timer if the component unmounts
+}, []);
+
+
   return (
-    <View>
-      <Text>Index</Text>
+    <View style = {styles.container}>
+      <Image source={require("../assets/images/logo.png")} style={styles.logo} />
+      <Text style = {styles.text}>SmartBite</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,  
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FCF4A3",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#3b8132",
+  },
+  logo : {
+    width: 200,
+    height: 200,
+    marginBottom: 50,
+  },
+});
 
 export default Index;
