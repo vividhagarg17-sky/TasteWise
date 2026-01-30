1582c304409a20a4ba3a3af4db2fbee7a9a1a7e3
Srishti Srivastava <srishti.school2010@gmail.com>
2026-01-30 19:44:44 +0530

    feat(ui): add plan selection screen and new plan pages
    Add interactive UI for selecting plans, routing to Digital Plan and Smart
    Plan screens, and implement new digitalplan.tsx and smartplan.tsx components.

---
+++ SmartBite/app/digitalplan.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DigitalPlan = () => {
+  return (
+    <View>
+      <Text>DigitalPlan</Text>
+    </View>
+  )
+}
+
+export default DigitalPlan
+
+const styles = StyleSheet.create({})
--- SmartBite/app/plans.tsx
+++ SmartBite/app/plans.tsx
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { router } from 'expo-router'; 
 
 const Plans = () => {
   return (
-    <View>
-      <Text>Plans</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Select a Plan</Text>
+      <View style={styles.plansContainer}>
+        <TouchableOpacity activeOpacity={0.7} onPress={() => router.replace('/digitalplan')}>
+          <Text style={styles.plantext}>Digital Plan - Free of Cost</Text> 
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.7}>
+          <Text style={styles.plantext}>Smart Plan - Just at ₹ 49 / month</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
 export default Plans
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start', 
+    paddingTop: 150,
+    backgroundColor: '#fcf4a3',
+  }, 
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 50, 
+  },
+  plansContainer: {
+    flex: 1,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    marginBottom: 300,
+  },
+  plantext: {
+    fontSize: 18,
+    marginVertical: 10,
+    backgroundColor: '#3b8132',
+    padding: 15,
+    borderRadius: 10,
+    textAlign: 'center',
+    marginBottom: 100,
+    color: '#fff',
+    width: 300,
+    height: 150,
+    textAlignVertical: 'center',
+    
+  },
+})
