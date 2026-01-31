42328a0b17a1e2838f4e4e3349111575bf50ef01
Srishti Srivastava <srishti.school2010@gmail.com>
2026-01-31 17:14:14 +0530

    another commit for digitalplan

--- SmartBite/app/digitalplan.tsx
+++ SmartBite/app/digitalplan.tsx
@@ -5,10 +5,16 @@ const DigitalPlan = () => {
   return (
     <View>
       <Text>DigitalPlan</Text>
+      <Text>{'\u2022'} Stealth Swaps</Text>
+      <Text>{'\u2022'}</Text>
+      <Text>{'\u2022'} Priority Support</Text>
+      <Text>{'\u2022'} Advanced Analytics</Text>
     </View>
   )
 }
 
 export default DigitalPlan
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
