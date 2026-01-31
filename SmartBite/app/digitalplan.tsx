cbca42537b22f35e923ca6681a40025cc16d0a31
Srishti Srivastava <srishti.school2010@gmail.com>
2026-01-31 17:36:29 +0530

    Added digital plan screen

--- SmartBite/app/digitalplan.tsx
+++ SmartBite/app/digitalplan.tsx
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { router } from 'expo-router';
 
 const DigitalPlan = () => {
   return (
-    <View>
-      <Text>DigitalPlan</Text>
-      <Text>{'\u2022'} Stealth Swaps</Text>
-      <Text>{'\u2022'}</Text>
-      <Text>{'\u2022'} Priority Support</Text>
-      <Text>{'\u2022'} Advanced Analytics</Text>
+    <View style={styles.container}>
+      <Text style = {styles.text}>DigitalPlan</Text>
+      <Text style = {styles.plantext}>{'\u2022'} Stealth Swaps</Text>
+      <Text style = {styles.plantext}>{'\u2022'} Cafe Suggestions </Text>
+      <Text style = {styles.plantext}>{'\u2022'} HealthCoins</Text>
+      <Text style = {styles.plantext}>{'\u2022'} Rewards: badges; certificates, digital prizes.</Text>
+      <TouchableOpacity activeOpacity={0.7} style={styles.digitalbtn}>
+        <Text style={styles.btntext} onPress={() => router.replace("/dashboard")}> Want to choose Digital Plan? Click here</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -16,5 +20,41 @@ const DigitalPlan = () => {
 export default DigitalPlan
 
 const styles = StyleSheet.create({
-
+container: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center', 
+  paddingTop: 50,
+  backgroundColor: '#fcf4a3',
+},
+text: { 
+  fontSize: 40,
+  color: '#fff',
+  marginBottom: 10,
+  backgroundColor: '#3b8132',
+  padding: 10,
+  borderRadius: 5,
+},
+plantext: {
+  fontSize: 18,
+  marginVertical: 10,
+  color: '#fff',
+  textAlign: 'center',
+  backgroundColor: '#3b8132',
+  padding: 10,
+  borderRadius: 10,
+  width: 300,
+  height: 70,
+  textAlignVertical: 'center',
+},
+digitalbtn: {
+  marginTop: 300, 
+  backgroundColor: '#49bba8', 
+  padding: 10,
+   borderRadius: 5,
+},
+btntext: {
+  color: '#fff',
+   fontSize: 18,
+}
 })
